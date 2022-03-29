d92c05762f9fa7762a5cb7bb39334d2d2438a46d
Enquestor <allenhu1029@gmail.com>
2022-03-30 00:00:18 +0800

    Ignore multiple builds bug on page switch

--- lib/providers/is_edit_mode_provider.dart
+++ lib/providers/is_edit_mode_provider.dart
@@ -1,7 +1,3 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:lazy1922/providers/selected_page_provider.dart';
 
-final isEditModeProvider = StateProvider<bool>((ref) {
-  ref.watch(selectedPageProvider);
-  return false;
-});
+final isEditModeProvider = StateProvider<bool>((ref) => false);
