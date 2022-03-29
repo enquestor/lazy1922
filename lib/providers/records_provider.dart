0d470b94d6b37853ec304dc3ec8388ea2fe2aec3
Enquestor <allenhu1029@gmail.com>
2022-03-29 15:48:29 +0800

    Sort messages on load

--- lib/providers/records_provider.dart
+++ lib/providers/records_provider.dart
@@ -6,7 +6,7 @@ import 'package:lazy1922/models/record.dart';
 class RecordsNotifier extends StateNotifier<List<Record>> {
   Box<List> get box => Hive.box<List>("records");
 
-  RecordsNotifier() : super((Hive.box<List>("records").get('records') ?? []).cast<Record>());
+  RecordsNotifier() : super((Hive.box<List>("records").get('records') ?? []).cast<Record>()..sort((a, b) => a.time.compareTo(b.time)));
 
   @override
   bool updateShouldNotify(List<Record> old, List<Record> current) => true;
